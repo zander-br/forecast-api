ea976519ea6b575bb19707a5e652ba9c43a23636
Anderson Santos <anderson.feitoza.santos@gmail.com>
2020-08-14 08:20:24 -0300

    feat: implementation create beach in controller

--- src/controllers/beaches.ts
+++ src/controllers/beaches.ts
@@ -1,10 +1,15 @@
 import { Controller, Post } from '@overnightjs/core';
 import { Request, Response } from 'express';
 
+import { Beach } from '@src/models/beach';
+
 @Controller('beaches')
 export class BeachesController {
   @Post('')
   public async create(req: Request, res: Response): Promise<void> {
-    res.status(201).send(req.body);
+    const beach = new Beach(req.body);
+    const result = await beach.save();
+
+    res.status(201).send(result);
   }
 }
