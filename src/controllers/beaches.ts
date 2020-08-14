8e2dd196de2e4008d105b48212b21aa0aba7d520
Anderson Santos <anderson.feitoza.santos@gmail.com>
2020-08-14 08:29:50 -0300

    feat: should return 422 where there is a validation error

--- src/controllers/beaches.ts
+++ src/controllers/beaches.ts
@@ -1,5 +1,6 @@
 import { Controller, Post } from '@overnightjs/core';
 import { Request, Response } from 'express';
+import mongoose from 'mongoose';
 
 import { Beach } from '@src/models/beach';
 
@@ -7,9 +8,17 @@ import { Beach } from '@src/models/beach';
 export class BeachesController {
   @Post('')
   public async create(req: Request, res: Response): Promise<void> {
-    const beach = new Beach(req.body);
-    const result = await beach.save();
+    try {
+      const beach = new Beach(req.body);
+      const result = await beach.save();
 
-    res.status(201).send(result);
+      res.status(201).send(result);
+    } catch (error) {
+      if (error instanceof mongoose.Error.ValidationError) {
+        res.status(422).send({ error: error.message });
+      } else {
+        res.status(500).send({ error: 'Internal Server Error' });
+      }
+    }
   }
 }
