54950b0a9449ff95336edbf6b21e4badc3bf163d
Anderson Santos <anderson.feitoza.santos@gmail.com>
2020-08-14 07:00:36 -0300

    chore: close server after all tests

--- test/jest-setup.ts
+++ test/jest-setup.ts
@@ -2,9 +2,13 @@ import supertest from 'supertest';
 
 import { SetupServer } from '@src/server';
 
-beforeAll(() => {
-  const server = new SetupServer();
-  server.init();
+let server: SetupServer;
+
+beforeAll(async () => {
+  server = new SetupServer();
+  await server.init();
 
   global.testRequest = supertest(server.getApp());
 });
+
+afterAll(async () => await server.close());
