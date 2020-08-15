7289d9b53e62bd8163a5b71e8651b9b1e5a410ce
Anderson Santos <anderson.feitoza.santos@gmail.com>
2020-08-15 09:04:39 -0300

    feat: should return 500 if something goes wrong during the processing

--- src/controllers/forecast.ts
+++ src/controllers/forecast.ts
@@ -13,9 +13,13 @@ export class ForecastController {
     _: Request,
     res: Response
   ): Promise<void> {
-    const beaches = await Beach.find({});
-    const forecastData = await forecast.processForecastForBeaches(beaches);
+    try {
+      const beaches = await Beach.find({});
+      const forecastData = await forecast.processForecastForBeaches(beaches);
 
-    res.status(200).send(forecastData);
+      res.status(200).send(forecastData);
+    } catch (error) {
+      res.status(500).send({ error: 'Something went wrong' });
+    }
   }
 }
