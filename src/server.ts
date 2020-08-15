988ff2b93c4599304a769c1fddeb2aeeaa286f96
Anderson Santos <anderson.feitoza.santos@gmail.com>
2020-08-15 09:23:03 -0300

    feat: add start method in server

--- src/server.ts
+++ src/server.ts
@@ -41,4 +41,10 @@ export class SetupServer extends Server {
   public getApp(): Application {
     return this.app;
   }
+
+  public start(): void {
+    this.app.listen(this.port, () => {
+      console.info('Server listening of port:', this.port);
+    });
+  }
 }
