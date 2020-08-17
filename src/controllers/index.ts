008f47c9aa821cc1d2e61bbabc54af70c2affd5d
Anderson Santos <anderson.feitoza.santos@gmail.com>
2020-08-17 06:15:43 -0300

    feat: should return 409 when the email already exists

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,5 +1,6 @@
 import { Response } from 'express';
 import mongoose, { Error } from 'mongoose';
+import { CUSTOM_VALIDATION } from '@src/models/user';
 
 export class BaseController {
   protected sendCreatedUpdateErrorResponse(
@@ -7,7 +8,15 @@ export class BaseController {
     error: mongoose.Error.ValidationError | Error
   ): void {
     if (error instanceof mongoose.Error.ValidationError) {
-      res.status(422).send({ code: 422, error: error.message });
+      const duplicatedKindErrors = Object.values(error.errors).filter(
+        (err) => err.kind === CUSTOM_VALIDATION.DUPLICATED
+      );
+
+      if (duplicatedKindErrors.length) {
+        res.status(409).send({ code: 409, error: error.message });
+      } else {
+        res.status(422).send({ code: 422, error: error.message });
+      }
     } else {
       res.status(500).send({ code: 500, error: 'Something went wrong!' });
     }
