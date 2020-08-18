5ab7fc7b3d476aae9d0b9dbc84b87d184f3e0575
Anderson Santos <anderson.feitoza.santos@gmail.com>
2020-08-18 06:39:41 -0300

    test: should return ANAUTHORIZED if the user is found but the password does not match

--- test/functional/users.test.ts
+++ test/functional/users.test.ts
@@ -87,5 +87,21 @@ describe('User funcional tests', () => {
 
       expect(response.status).toBe(401);
     });
+
+    it('should return ANAUTHORIZED if the user is found but the password does not match', async () => {
+      const newUser = {
+        name: 'John Doe',
+        email: 'john@mail.com',
+        password: '1234',
+      };
+
+      await new User(newUser).save();
+
+      const response = await global.testRequest
+        .post('/users/authenticate')
+        .send({ email: newUser.email, password: 'different password' });
+
+      expect(response.status).toBe(401);
+    });
   });
 });
