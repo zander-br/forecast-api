0ccffc39eb593fc33e9261a9ce534ef777a19794
Anderson Santos <anderson.feitoza.santos@gmail.com>
2020-08-19 06:35:43 -0300

    test: should return UNAUTHORIZED if there is a pronlem on the token verification

--- src/middlewares/__test__/auth.test.ts
+++ src/middlewares/__test__/auth.test.ts
@@ -18,4 +18,28 @@ describe('AuthMiddleware', () => {
 
     expect(nextFake).toHaveBeenCalled();
   });
+
+  it('should return UNAUTHORIZED if there is a pronlem on the token verification', () => {
+    const reqFake = {
+      headers: {
+        'x-access-token': 'invalid token',
+      },
+    };
+
+    const sendMock = jest.fn();
+    const resFake = {
+      status: jest.fn(() => ({
+        send: sendMock,
+      })),
+    };
+
+    const nextFake = jest.fn();
+
+    authMiddleware(reqFake, resFake as object, nextFake);
+    expect(resFake.status).toHaveBeenCalledWith(401);
+    expect(sendMock).toHaveBeenCalledWith({
+      code: 401,
+      error: 'jwt malformed',
+    });
+  });
 });
